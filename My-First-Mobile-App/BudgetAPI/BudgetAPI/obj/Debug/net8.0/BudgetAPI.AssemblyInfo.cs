c0a630f6578808d812efd63fa97af466fa1185cf
Trevor Damoyi <trevordamoyi307@gmail.com>
2024-08-14 14:43:31 +0200

    updates

--- My-First-Mobile-App/BudgetAPI/BudgetAPI/obj/Debug/net8.0/BudgetAPI.AssemblyInfo.cs
+++ My-First-Mobile-App/BudgetAPI/BudgetAPI/obj/Debug/net8.0/BudgetAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaff295fe1de27e2fe591cae6c0d7b4ba75ce30f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2faefc6536cb8c4716d757ed20b56309787da602")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
